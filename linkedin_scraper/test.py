fd1498397792b2ecc39718830ab94f5182eb21b8
Erik Pereda <erikper0604@gmail.com>
2026-04-03 12:31:36 -0400

    Adjust mouse movement coordinates in test.py

--- linkedin_scraper/test.py
+++ linkedin_scraper/test.py
@@ -3,5 +3,5 @@ import time
 
 print("Moving to first profile in 3 seconds...")
 time.sleep(3)
-pyautogui.moveTo(360, 365, duration=1)
+pyautogui.moveTo(427, 347, duration=1)
 print("Is the mouse on Maylin Barcena's name? (y/n)")
